0b9c39adb2a0aaa6c483a679546f05ba86a35a65
oscarmartinezz <oscarmartinezlopez99@gmail.com>
2021-12-04 22:26:25 +0100

    Clase Correo creada disponible para uso

--- automatizadorEmails/Correo.class.php
+++ automatizadorEmails/Correo.class.php
@@ -1,16 +1,20 @@
 <?php
     Class Correo{
-        private static $cabecera="From: contacto@uchatech.es\r\n";
-        
-        public static function enviarCorreo($destinatario, $asunto, $mensaje) {
+        //Definimos un atributo estático para la cabecera
+        private static $cabecera="MIME-Version: 1.0"."\r\n"."Content-type: text/plain; charset=utf-8"."\r\n"."To: "."\r\n"."From: contacto@uchatech.es"."\r\n";
+
+        public static function enviarCorreo($destinatario, $asunto, $mensaje) { //Creamos la función para poder enviar correos
             ini_set( 'display_errors', 1 );
             error_reporting( E_ALL );
-            if(is_array($destinatario)) //Comprobamos si el destinatario es un array
-                $destinatario=implode(",", $destinatario); //De serlo, lo convertimos en un array que divimos por comas
+            if(!is_array($destinatario)) //Comprobamos si el destinatario no es un array, que se dará el caso cuando solo se pase un destinatario
+                $destinatario=array($destinatario); //Si es asi creamos un array con una sola posición para que no nos de problemas en el bucle
+
             $mensaje=str_replace("<br/>","\n",$mensaje); //En el mensaje sustituímos los <br/> por \n para que se produzcan saltos de línea
-            self::$cabecera.="Bcc: $destinatario\r\n";
-            mail($destinatario,$asunto,$mensaje, self::$cabecera); //Llamamos a la función mail para enviar el correo
-            echo "El email se ha enviado correctamente";
+            
+            //Hacemos un bucle for para recorrer el array de destinatarios y llamamos a la función mail para cada destinatario y mandarle un correo individual a cada uno
+            for($i=0; $i<count($destinatario); $i++){
+                mail($destinatario[$i],$asunto,$mensaje, self::$cabecera); //Llamamos a la función mail para enviar el correo y le pasamos los parámetros necesarios
+            }
         }
     }
 ?>
--- automatizadorEmails/ejemploFuncionEnvioEmail.php
+++ automatizadorEmails/ejemploFuncionEnvioEmail.php
@@ -1,8 +1,10 @@
 <?php
+    //Esto es un ejemplo de lo que deberías de hacer para poder enviar correos correctamente usando el objeto Correo
+
     //Incluímos la clase al principio de la página
     include_once("Correo.class.php");
 
-    //Si tienes que enviarle el correo a varios destinatarios, guardarlos en un array que pasarás cuando llames a la función al final
+    //Si quieres enviarle el correo a varios destinatarios, guardarlos en un array que pasarás como parámetro cuando llames a la función al final
     $destinatario=array("oscarmartinezlopez99@gmail.com", "frosty.oscar.9@gmail.com");
 
     //Si solo es un destinatario sería más sencillo
@@ -12,8 +14,8 @@
     $asunto="Correo de prueba";
 
     //Crea también una variable para guardar el mensaje que quieres enviar
-    //IMPORTANTE: si quieres hacer un salto de línea escribe <br/>, mira el ejemplo abajo
-    $mensaje="Esto es un mensaje de prueba para comprobar que el método de la clase correo funciona correctamente<br/><br/>Muchas gracias,<br/>TechUcha.";
+    //IMPORTANTE: si quieres hacer un salto de línea escribe <br/>, como en el ejemplo de la siguiente línea
+    $mensaje="Esto es un mensaje de prueba para comprobar que el método de la clase Correo funciona correctamente.<br/><br/>Muchas gracias,<br/>TechUcha.";
 
     //Finalmente llamas a la función de esta forma pasándole los parámetros en este orden
     Correo::enviarCorreo($destinatario, $asunto, $mensaje);
