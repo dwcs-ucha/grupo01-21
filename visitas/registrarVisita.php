3e2f1c84d4e78c34bdcfbd59040c2cf31e15eca2
Fran Gavilán Bellón <94417800+BeGavilRon2k1@users.noreply.github.com>
2021-12-11 13:57:25 +0100

    Add files via upload

---
+++ visitas/registrarVisita.php
@@ -0,0 +1,24 @@
+<?php
+$cookieUsuario = "visitas";
+$fechayHora = date('D d / F / Y / H:i:s');
+
+if (isset($_COOKIE['fechas'])) {
+
+    $fechas = explode(',', $_COOKIE['fechas']);
+} else {
+
+    $fechas = array();
+}
+
+if (!empty($_COOKIE[$cookieUsuario])) {
+
+    $valorcookie = intval($_COOKIE[$cookieUsuario]) + 1;
+    setcookie($cookieUsuario, $valorcookie);
+    array_push($fechas, $_SESSION['usuario'] . " / " . $fechayHora);
+    $stringFechas = implode(',', $fechas);
+    setcookie('fechas', $stringFechas);
+} else {
+    //Cookie encargada de las visitas (seguimiento)
+    setcookie($cookieUsuario, 1);
+    setcookie('fechas', $_SESSION['usuario'] . " / " . $fechayHora);
+}
